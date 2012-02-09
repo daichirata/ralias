d5579316837bdf91a4d0fa7a52d78e1f10cbd513
Daic_h <bunny.hop.md@gmail.com>
2012-02-10 00:10:30 +0900

    add built in commands

--- lib/ralias/command.rb
+++ lib/ralias/command.rb
@@ -2,8 +2,16 @@ module Ralias
   module Command
     extend Color
 
+    def self.rc_path
+      File.expand_path("~/.raliasrc")
+    end
+
     def self.commands
       @@commands ||= {}
+    end
+
+    def self.built_in_commands
+      @@built_in_commands ||= []
     end
 
     def self.define(name, &block)
@@ -14,7 +22,6 @@ module Ralias
       require 'ralias/define_commands'
 
       begin
-        rc_path = File.expand_path("~/.raliasrc")
         class_eval(File.open(rc_path).read) if File.exist?(rc_path)
         with_color(:cyan) { "load ~/.raliasrc" }
       rescue
@@ -27,15 +34,23 @@ module Ralias
       required_args = cmd.parameters
 
       unless required_args.size == args.size
-        with_color(:red){ "wrong number of arguments (#{args.size} for #{required_args.size}) (ArgumentError)" }
+        with_color(:red) do
+          "wrong number of arguments (#{args.size} for #{required_args.size}) (ArgumentError)"
+        end
+
         required_args.each do |arg|
           with_color { "argumets: " + arg[1].to_s }
         end
+        raise CommandNotFound
       else
-        system cmd.call(*args)
+        if built_in_commands.include?(_cmd)
+          cmd.call(*args)
+        else
+          system cmd.call(*args)
+        end
       end
 
-    rescue NoMethodError
+    rescue
       with_color(:red) { "command not found." }
     end
   end
--- lib/ralias/define_commands.rb
+++ lib/ralias/define_commands.rb
@@ -1,18 +1,43 @@
 module Ralias::Command
   define("help") do
-    <<-HELP
-
-
-
-    HELP
+    with_color(:red) do
+      <<-HELP
+help             -> print help
+init             -> create ~/.raliasrc
+new              -> create new aliase command
+list             -> print defined aliase command
+HELP
+    end
   end
+  built_in_commands << "help"
 
   define("init") do
+    unless File.exist?(rc_path)
+      File.open(rc_path, "w") do |rc_file|
+        rc_file.puts <<-'EXAMPLE'
+#  Example
+#
+#  normal alias definition
+#
+#    define("lsA") { "ls -al" }
+#
+#  if you want to use the arguments to the alias
+#
+#    define("github") do |user_name, repository|
+#      "git clone https://github.com/#{user_name}/#{repository}.git"
+#    end
+        EXAMPLE
+      end
+      with_color(:yellow) { "create file #{rc_path}" }
+    end
   end
+  built_in_commands << "init"
 
   define("new") do |name, alias_command|
   end
+  built_in_commands << "new"
 
   define("list") do
   end
+  built_in_commands << "list"
 end
